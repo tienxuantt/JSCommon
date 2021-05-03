9e051fa69ad6f427690ee531da183c718623ec13
tienxuantt <tienxuantt@gmail.com>
2021-05-03 22:55:44 +0700

    'add'

---
+++ commonjs.js
@@ -0,0 +1,38 @@
+// Format số tiền
+function formatMoney(money){
+    if(!isNaN(money)){
+        return money.toString().replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, "$1.");
+    }else{
+        return money;
+    }
+}
+
+// Format ngày tháng
+function formatDate(date){
+    let year = date.getFullYear().toString(),
+        month = (date.getMonth() + 1).toString().padStart(2, '0'),
+        day = date.getDate().toString().padStart(2, '0');
+
+    return `${day}/${month}/${year}`;
+}
+
+// Validate ngày tháng nhập vào
+function validateDate(date){
+    let regex = new RegExp("([0-9]{4}[-](0[1-9]|1[0-2])[-]([0-2]{1}[0-9]{1}|3[0-1]{1})|([0-2]{1}[0-9]{1}|3[0-1]{1})[-](0[1-9]|1[0-2])[-][0-9]{4})");
+    
+    return regex.test(date);
+}
+
+// Validate số điện thoại
+function validateDate(phone){
+    let regex = /((09|03|07|08|05)+([0-9]{8})\b)/g;
+    
+    return regex.test(phone);
+}
+
+// Validate email
+function validateEmail(email) {
+    let regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return regex.test(String(email).toLowerCase());
+}
